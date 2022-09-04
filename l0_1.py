b07c5023f2c595837499ea950edef0f8a5824504
pacos47 <pacos47@gmail.com>
2022-09-04 20:55:14 +0300

    from term

--- l0_1.py
+++ l0_1.py
@@ -1,2 +1,11 @@
-input("What's your name? ")
+'''input("What's your name? ")
 print("hello, world")
+#better
+name=input("What's your name ? ")
+print("Hello"+name)
+print("Hello",name)'''
+name=input("What's your name ? ")
+print("Hello " , end="") ; print(name)
+#most elegant
+print(f"Hello , {name}")
+
